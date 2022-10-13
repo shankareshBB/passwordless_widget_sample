e2a038f7603aea0e481d2d0b5489d0aecabe6b30
shankareshbb <shankaresh@mollatech.com>
2022-10-13 10:41:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(8087, () => {
   console.log('passwordless Widget sample running on Port ' + 8087);
